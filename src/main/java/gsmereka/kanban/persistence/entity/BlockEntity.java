8585055d0ac0717440b01ce69f192baabcd0a76f
FarofaFaction <gabrielsmereka200@gmail.com>
2025-03-29 18:38:05 -0300

    feat: add Entity's and BoardColumnKindEnum.java

---
+++ src/main/java/gsmereka/kanban/persistence/entity/BlockEntity.java
@@ -0,0 +1,16 @@
+package gsmereka.kanban.persistence.entity;
+
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+
+@Data
+public class BlockEntity {
+
+    private Long id;
+    private OffsetDateTime blockedAt;
+    private String blockReason;
+    private OffsetDateTime unblockedAt;
+    private String unblockReason;
+
+}
