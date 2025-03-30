a72dd228dea70d29e3d1844304515f1108df9412
FarofaFaction <gabrielsmereka200@gmail.com>
2025-03-29 23:16:01 -0300

    feat: add Services and Exceptions

---
+++ src/main/java/gsmereka/kanban/exception/CardBlockedException.java
@@ -0,0 +1,8 @@
+package gsmereka.kanban.exception;
+
+public class CardBlockedException extends RuntimeException{
+
+    public CardBlockedException(final String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/gsmereka/kanban/service/CardQueryService.java
@@ -0,0 +1,21 @@
+package gsmereka.kanban.service;
+
+import gsmereka.kanban.dto.CardDetailsDTO;
+import gsmereka.kanban.persistence.dao.CardDAO;
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class CardQueryService {
+
+    private final Connection connection;
+
+    public Optional<CardDetailsDTO> findById(final Long id) throws SQLException {
+        var dao = new CardDAO(connection);
+        return dao.findById(id);
+    }
+
+}
