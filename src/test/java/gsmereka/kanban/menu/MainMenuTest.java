b29308580e4020246784d7d35cb4bff3e5e1e850
FarofaFaction <gabrielsmereka200@gmail.com>
2025-04-01 18:17:28 -0300

    fix: Refactoring MainMenuTest.java

--- src/test/java/gsmereka/kanban/menu/MainMenuTest.java
+++ src/test/java/gsmereka/kanban/menu/MainMenuTest.java
@@ -12,26 +12,42 @@ import java.util.Scanner;
 
 import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
 import static gsmereka.kanban.persistence.config.ConnectionConfig.getConnection;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
 
 public class MainMenuTest {
 
     @Test
     public void testCreateBoard() throws SQLException {
+        runTestWithInput("5\n");
+    }
+
+    @Test
+    public void testAddBoard() throws SQLException {
+        runTestWithInput("1\nBoard\n0\nColuna Inicial\nColuna Final\nColuna Cancelamento\n5");  // Exemplo de outro input
+    }
+
+    private void runTestWithInput(String simulatedInput) throws SQLException {
         ConnectionConfig.configConnection("jdbc:mysql://localhost:9898/KanbanTest", "KanbanTest", "KanbanTest");
-        try(var connection = getConnection()){
+        try (var connection = getConnection()) {
             new MigrationStrategy(connection).executeMigration("/db/changelog/db.changelog-for-tests.yml");
         }
-//        String simulatedInput = "1\nBoard\n0\nColuna Inicial\nColuna Final\nColuna Cancelamento\n5";
-        String simulatedInput = "2\n5\n";
         InputStream in = new ByteArrayInputStream(simulatedInput.getBytes());
         System.setIn(in);
 
+        boolean exceptionOccurred = false;
         try {
             new MainMenu().execute();
         } catch (SQLException e) {
+            exceptionOccurred = true;
             fail("SQLException foi lançada: " + e.getMessage());
+        } catch (Exception e) {
+            exceptionOccurred = true;
+            fail("Uma exceção inesperada foi lançada: " + e.getMessage());
         }
+        assertFalse(exceptionOccurred, "O teste falhou devido a uma exceção.");
     }
 }
